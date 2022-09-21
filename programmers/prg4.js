df8de776943a38cd8a6864b05b921cfb00ab0457
이주연 <jooyeon@MacBook-Pronaekkeo.local>
2022-09-21 23:31:08 +0900

    위장

--- programmers/prg4.js
+++ programmers/prg4.js
@@ -1,53 +1,10 @@
-const clothes = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"], ["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]];
+var clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"],["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]];
 
 var answer = 0;
+var obj = {};
 
-let count = clothes.length;  //전체 옷의 개수
-
-
-//옷 종류
-var clothes_type = [];   
-for ( var i = 0; i < clothes.length; i++) {
-    clothes_type.push(clothes[i][1]);
+for(let i = 0; i < clothes.length; i++) {
+    obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
 }
-const real_type = [...new Set(clothes_type)];
 
-
-//각 종류별로 몇개씩 있는지
-var li = [];
-var i = 0;
-while (i < real_type.length) {
-    var count_clothes = 0;
-    for (var j = 0; j < clothes.length; j++) {    
-        if (real_type[i] === clothes[j][1]) {
-            count_clothes += 1;            
-        }
-    }
-    li.push(count_clothes);
-    i++;
-}
-const obj = {};
-for (var i = 0; i < li.length; i++) {
-    obj[real_type[i]] = li[i];
-}
 console.log(obj);
-
-
-
-
-
-
-
-
-var m = 1;
-for (var i = 0; i < li.length; i++) {
-    m *= li[i];
-}
-
-answer = m + count;
-
-if (real_type.length === 1) {
-    answer = count;
-}
-
-console.log(answer);
