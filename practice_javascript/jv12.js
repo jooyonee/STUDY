21303c43c2d645ecf449bd7c4c090e1cda1fc423
jooyonee <isyawn@naver.com>
2022-10-25 21:26:42 +0900

    구구단

---
+++ practice_javascript/jv12.js
@@ -0,0 +1,50 @@
+// const readline = require("readline");
+ 
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// let num1 = Math.floor(Math.random()*9 +1);
+// let num2 = Math.floor(Math.random()*9 +1);
+// console.log(num1, '*', num2, '= ?');
+
+// rl.on("answer", (answer) => {
+//     if (num1 * num2 === answer) {
+//         console.log("정답입니다.");
+//     }
+//     else {
+//         console.log("틀렸습니다.");
+//     }
+//     rl.close();
+// });
+
+// rl.on('close', () => {
+//         process.exit();
+// })
+
+
+const readline = require("readline");
+ 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let num1 = Math.floor(Math.random()*9 +1);
+let num2 = Math.floor(Math.random()*9 +1);
+console.log(num1, '*', num2, '= ?');
+
+rl.on("line", (line) => {
+    if(parseInt(num1 * num2) === parseInt(line)) {
+        console.log("정답입니다."); 
+    } else {
+        console.log("웅앵웅");
+    }
+    
+    rl.close();
+});
+ 
+rl.on('close', () => {
+        process.exit();
+})
