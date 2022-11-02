1a141a146c134f6941f2c9ad2b3d65d74eb09e48
jooyonee <isyawn@naver.com>
2022-11-02 23:16:32 +0900

    제로초_웹게임_숫자야구

--- practice_javascript/jv15.js
+++ practice_javascript/jv15.js
@@ -1,5 +1,6 @@
 var body = document.body;
 
+
 let random_num_arr = [];
 for (i=0; i<4; i++) {
   var num = Math.ceil(Math.random() * 9);
@@ -9,6 +10,7 @@ for (i=0; i<4; i++) {
     i--;
   }
 }
+var count = 0;  //틀린 횟수 카운트
 
 var conclu = document.createElement('h1');
 document.body.append(conclu);
@@ -45,7 +47,7 @@ form_tag.addEventListener('submit', function (event1) {
     // inputt.textContent = typeof(input_blank.value) + '(' + input_blank.value + ')';
     // document.body.append(inputt);
 
-    console.log(input_blank.value === random_num_arr.join(''));
+    // console.log(input_blank.value === random_num_arr.join(''));
     
     var inputt = input_blank.value;
     var answer = inputt.split('');
@@ -53,7 +55,7 @@ form_tag.addEventListener('submit', function (event1) {
     console.log(answer);
     console.log(random_num_arr);  //자료형이 다름!
     
-
+    
 
     if (inputt === random_num_arr.join('')) {
         conclu.textContent = '맞췄습니다.';
@@ -70,8 +72,7 @@ form_tag.addEventListener('submit', function (event1) {
             word.textContent = "뭐게요 맞춰봐요  *hint : " + random_num_arr;
         input_blank.value = '';
         input_blank.focus();
-    } else {
-        
+    } else { 
 
         var answer = inputt.split('');
         var strike = 0;
@@ -84,11 +85,30 @@ form_tag.addEventListener('submit', function (event1) {
                 ball++;
             }
         }
-        conclu.textContent = '아닙니다. ㅡㅡ';
+        count++;
+        conclu.textContent = '아닙니다. ㅡㅡ (틀린횟수 : ' + count + ')';
         conclu2.textContent = 'strike : ' + strike + ' / ball : ' + ball;
+       
         
         input_blank.value = '';
         input_blank.focus();
+
+        
+        if (count >= 10) {
+            conclu.textContent = '답은 '+ random_num_arr.join('') +'입니다.';
+            random_num_arr = [];
+            for (i=0; i<4; i++) {
+                var num = Math.ceil(Math.random() * 9);
+                if (random_num_arr.indexOf(num) === -1) {
+                    random_num_arr.push(num);
+                } else {
+                    i--;
+                }
+            }
+            word.textContent = "새로운 문제 : 뭐게요 맞춰봐요  *hint : " + random_num_arr;
+            count = 0;
+        }
+
     }
 
 });
