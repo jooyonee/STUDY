f68c60291b59a7fdeaaa2fa1d194d308764075f5
jooyonee <isyawn@naver.com>
2022-11-02 22:13:58 +0900

    제로초_웹게임_숫자야구

--- practice_javascript/jv15.js
+++ practice_javascript/jv15.js
@@ -13,17 +13,16 @@ for (i=0; i<4; i++) {
 var conclu = document.createElement('h1');
 document.body.append(conclu);
 
+var conclu2 = document.createElement('h2');
+document.body.append(conclu2);
+
 var word = document.createElement('div');
 word.textContent = "뭐게요 맞춰봐요  *hint : " + random_num_arr;
 document.body.append(word);
 
-console.log(random_num);
-
 
 var form_tag = document.createElement('form');
 document.body.append(form_tag);
-
-console.log(random_num);
 
 
 var input_blank = document.createElement('input');
@@ -32,14 +31,11 @@ input_blank.type = 'text';
 input_blank.manLength = 4;
 input_blank.focus();
 
-console.log(random_num);
-
 
 var input_button = document.createElement('button');
 input_button.textContent = 'enter';
 form_tag.append(input_button);
 
-console.log(random_num);
 
 
 form_tag.addEventListener('submit', function (event1) {
@@ -52,8 +48,16 @@ form_tag.addEventListener('submit', function (event1) {
     console.log(input_blank.value === random_num_arr.join(''));
     
     var inputt = input_blank.value;
+    var answer = inputt.split('');
+
+    console.log(answer);
+    console.log(random_num_arr);  //자료형이 다름!
+    
+
+
     if (inputt === random_num_arr.join('')) {
         conclu.textContent = '맞췄습니다.';
+        conclu2.textContent = '';
             random_num_arr = [];
             for (i=0; i<4; i++) {
                 var num = Math.ceil(Math.random() * 9);
@@ -67,21 +71,21 @@ form_tag.addEventListener('submit', function (event1) {
         input_blank.value = '';
         input_blank.focus();
     } else {
-        conclu.textContent = '아닙니다. ㅡㅡ';
+        
 
         var answer = inputt.split('');
         var strike = 0;
         var ball = 0;
 
         for (var i = 0; i < 4; i++) {
-            if (answer[i] === random_num_arr[i]) {
+            if (inputt[i] == random_num_arr[i]) {
                 strike++;
-            } else if (random_num_arr.includes(answer[i])) {
+            } else if (random_num_arr.includes(Number(answer[i]))) {
                 ball++;
             }
         }
-        
-
+        conclu.textContent = '아닙니다. ㅡㅡ';
+        conclu2.textContent = 'strike : ' + strike + ' / ball : ' + ball;
         
         input_blank.value = '';
         input_blank.focus();
