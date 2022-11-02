7bf5b768eaa5d6148a5a65e42e42584371f82698
jooyonee <isyawn@naver.com>
2022-11-02 21:59:17 +0900

    제로초_웹게임_숫자야구

--- practice_javascript/jv15.js
+++ practice_javascript/jv15.js
@@ -9,13 +9,12 @@ for (i=0; i<4; i++) {
     i--;
   }
 }
-var random_num = random_num_arr.join('');
 
 var conclu = document.createElement('h1');
 document.body.append(conclu);
 
 var word = document.createElement('div');
-word.textContent = "뭐게요 맞춰봐요 / 답 : " + random_num + ' ' + typeof(random_num);
+word.textContent = "뭐게요 맞춰봐요  *hint : " + random_num_arr;
 document.body.append(word);
 
 console.log(random_num);
@@ -52,8 +51,8 @@ form_tag.addEventListener('submit', function (event1) {
 
     console.log(input_blank.value === random_num_arr.join(''));
     
-
-    if (input_blank.value === random_num_arr.join('')) {
+    var inputt = input_blank.value;
+    if (inputt === random_num_arr.join('')) {
         conclu.textContent = '맞췄습니다.';
             random_num_arr = [];
             for (i=0; i<4; i++) {
@@ -64,13 +63,26 @@ form_tag.addEventListener('submit', function (event1) {
                     i--;
                 }
             }
-            var random_num = random_num_arr.join('');
-            word.textContent = "뭐게요 맞춰봐요" + random_num;
+            word.textContent = "뭐게요 맞춰봐요  *hint : " + random_num_arr;
         input_blank.value = '';
         input_blank.focus();
-        condi = false;
     } else {
         conclu.textContent = '아닙니다. ㅡㅡ';
+
+        var answer = inputt.split('');
+        var strike = 0;
+        var ball = 0;
+
+        for (var i = 0; i < 4; i++) {
+            if (answer[i] === random_num_arr[i]) {
+                strike++;
+            } else if (random_num_arr.includes(answer[i])) {
+                ball++;
+            }
+        }
+        
+
+        
         input_blank.value = '';
         input_blank.focus();
     }
