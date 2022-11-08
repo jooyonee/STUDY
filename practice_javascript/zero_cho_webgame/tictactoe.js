341405d76722a6294a16c224d03627be43e69732
jooyonee <isyawn@naver.com>
2022-11-08 22:41:42 +0900

    Event Bubbling

--- practice_javascript/zero_cho_webgame/tictactoe.js
+++ practice_javascript/zero_cho_webgame/tictactoe.js
@@ -1,13 +1,26 @@
 
 let turn = 'O';
 
+//함수 분리
+const callback = (event) => {
+    if(event.target.textContent) return;
+        event.target.textContent = turn;
+        if (turn == 'O') {
+            turn = 'X';
+        } else if (turn == 'X') {
+            turn = 'O';
+        }
+};
+// event.target = 이벤트 발생한 애
+// event.current.target = EventListener 붙인 애
+
+
 const data = [];
 for (let i = 0; i < 3; i++) {
     data.push([]);
 }  //테이블 형성
 
 //table > tr > td
-const { body, createElement } = document;
 const $table = document.createElement('table');
 const $result = document.createElement('div')
 
@@ -18,27 +31,18 @@ for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
         const $td = document.createElement('td');
 
-        $td.addEventListener('click', (event) => {
-
-            if(event.target.textContent) return;
-            event.target.textContent = turn;
-            if (turn == 'O') {
-                turn = 'X';
-            } else if (turn == 'X') {
-                turn = 'O';
-            }
-
-        });
         cells.push($td);
         $tr.append($td);
     }
     rows.push(cells);
     $table.append($tr);
 }
+
+$table.addEventListener('click', callback);  
+
 document.body.append($table);
 document.body.append($result);
 
-console.log(rows);
 
 
 
