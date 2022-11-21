97c86925cf2cfe004145d219ed1ce43a7ca9cbdc
jooyonee <isyawn@naver.com>
2022-11-21 20:38:02 +0900

    제로초 가위바위보

--- practice_javascript/zero_cho_webgame/rsp/rsp.js
+++ practice_javascript/zero_cho_webgame/rsp/rsp.js
@@ -8,3 +8,16 @@ const IMG_url = './rsp.png';
 
 computer.style.background = `url(${IMG_url}) 0 0`;
 computer.style.backgroundSize = 'auto 200px';
+
+const rsp_x = {
+    scissors : '0',
+    rock : '-220px',
+    paper : '-440px',
+};
+
+
+let display = 0;
+setInterval(() =>  {
+    computer.style.background = `url(${IMG_url}) 0 0`;
+    computer.style.backgroundSize = 'auto 200px';
+}, 50);
