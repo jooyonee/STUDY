427726557d088470da77f73a5e92cf84ee4deda7
jooyonee <isyawn@naver.com>
2022-11-27 23:57:47 +0900

    카드뒤집기 게임

--- practice_javascript/zero_cho_webgame/card/card.js
+++ practice_javascript/zero_cho_webgame/card/card.js
@@ -1,8 +1,12 @@
 const total = 12;
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-let colorcopy = colors.concat(colors);  //배열복사해서 뒤에 붙여넣기(*2)
 
 let shuffled = [];
+let colorcopy = colors.concat(colors);  //배열복사해서 뒤에 붙여넣기(*2)
+let clickable = true;
+let correct = [];
+let clicked = [];
+
 
 //  카드 섞는 함수
 function shuffle() {   
@@ -12,10 +16,10 @@ function shuffle() {
     }
 }
 
-const $wrapper = document.querySelector('#wrapper');
 
 //  카드 1개 생성 함수
 function createCard(i) {
+    clickable = false;
     const card = document.createElement('div');
     card.className = 'card';
     const card_inner = document.createElement('div');
@@ -24,8 +28,7 @@ function createCard(i) {
     card_front.className = 'card_front';
     const card_back = document.createElement('div');
     card_back.className = 'card_back';
-    // card_back.style.backgroundColor = shuffled[i];
-    $wrapper.appendChild(card);
+    card_back.style.backgroundColor = shuffled[i];
     card_inner.appendChild(card_front);
     card_inner.appendChild(card_back);
     card.appendChild(card_inner);
@@ -33,11 +36,55 @@ function createCard(i) {
     return card;    
 }
 
+const $wrapper = document.querySelector('#wrapper');
 
-shuffle();
-console.log(shuffled);
+// 카드 두 장(한 쌍) 클릭할때마다
 
-for (let i = 0; i < total; i++) {
-    const $card = createCard(i);
-    $wrapper.appendChild($card);
+function onClickCard() {
+
+    this.classList.toggle('flipped');
+    clicked.push(this);  // this = card
+    if (clicked.length !== 2) {
+        return;  // 2장 선택 안 했으면 대기
+    }
+    const firstcard = clicked[0].querySelector('card_back').style.backgroundColor;
+    const secondcard = clicked[1].querySelector('card_back').style.backgroundColor;
+
+    if (firstcard == secondcard) {
+        correct.push(clicked[0]);
+        correct.push(clicked[1]);
+        clicked = [];
+        return;
+    }
+    clicked[0].classList.remove('flipped');
+    clicked[1].classList.remove('flipped');
+    clicked = []; // clicked 배열 리셋
 }
+
+
+function startGame() {
+    clickable = false;
+    shuffle();
+    console.log(shuffled);
+
+    for (let i = 0; i < total; i++) {
+        const card = createCard(i);
+        card.addEventListener('click', onClickCard);
+        $wrapper.appendChild(card);
+    }
+
+    document.querySelectorAll('.card').forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('flipped');
+        }, 1000 + 100 * index);
+    });
+
+    setTimeout(() => {
+        document.querySelectorAll('.card').forEach((card) => {
+            card.classList.remove('flipped');
+        });
+    }, 5000);
+    };
+
+
+startGame();
